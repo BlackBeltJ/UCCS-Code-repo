76aacc99436fb39ff322043d5dda361ac046c0d3
Joshua Edwards <BlackBeltJJE@gmail.com>
2023-09-02 12:31:58 -0600

    Update fig02_01.c
    GE02. Added algorithm to check for valid user input. Added functions to calculate the area and the average of the width and height.

--- CS2060ClassCodeFall2023-main/CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
+++ CS2060ClassCodeFall2023-main/CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
@@ -1,10 +1,76 @@
 // fig02_01.c
 // A first program in C.
 #include <stdio.h>
+#include <stdbool.h>
 
 // function main begins program execution 
 int main(void) {
-   printf("Welcome to C!\n");
+	int length = 0;
+	int width = 0;
+	int area = 0;
+	int scanfReturn = 0;
+	int scanfReturn2 = 0;
+
+	puts("Enter the length: ");
+	// printf("%s", "Enter the length");
+	scanfReturn = scanf("%d", &length);
+
+	// algorithm to check valid input
+	bool isValidInput = false;
+	while ((getchar() != '\n')); // clears the input buffer
+	while (isValidInput == false)
+	{
+		if (scanfReturn == 1)
+		{
+			// if scanf returns a 1, that means that data was successfully read (the user entered an integer)
+			/* this is where I would check that the user entered in a valid value
+			considering the range of acceptable values,	but for the purposes of
+			this GE, I am only concerned with checking if the user entered in the
+			correct data type */
+			isValidInput = true;
+		}
+		else
+		{
+			// prompt the user for an integer again if they did not enter one initially
+			puts("Please enter an integer, try again.");
+			scanfReturn = scanf("%d", &length);
+			while ((getchar() != '\n')); // clear the input buffer again
+		}
+	}
+
+	puts("Enter the width: ");
+	scanfReturn2 = scanf("%d", &width);
+
+	// algorithm to check valid input
+	isValidInput = false;
+	while ((getchar() != '\n')); // clears the input buffer
+	while (isValidInput == false)
+	{
+		if (scanfReturn2 == 1)
+		{
+			// if scanf returns a 1, that means that data was successfully read (the user entered an integer)
+			/* this is where I would check that the user entered in a valid value
+			considering the range of acceptable values,	but for the purposes of
+			this GE, I am only concerned with checking if the user entered in the
+			correct data type */
+			isValidInput = true;
+		}
+		else
+		{
+			// prompt the user for an integer again if they did not enter one initially
+			puts("Please enter an integer, try again.");
+			scanfReturn2 = scanf("%d", &width);
+			while ((getchar() != '\n')); // clear the input buffer again
+		}
+	}
+
+	area = width * length;
+	printf("Length: %d, Width: %d, and the area is: %d", length, width, area);
+
+	double averageResult = (double) (length + width) / 2;
+	printf("\nAverage of length and width: %.1f", averageResult);
+
+    // printf("Welcome to C!\n");
 } // end function main 
 
 
