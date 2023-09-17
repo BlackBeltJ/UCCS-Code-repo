7ede88c37cd5b8a2d8554afd2d5ddb2042f4916b
Joshua Edwards <BlackBeltJJE@gmail.com>
2023-09-17 17:58:43 -0600

    Created AirUCCS project file

---
+++ CS2060ClassCode/if loop test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void){
+    int number = 2;
+
+    if (number > 1)
+    {
+        puts("Number is greater than 1");
+    }
+    else if (number > 0)
+    {
+        puts("Number is greater than 0");
+    }
+}//f
