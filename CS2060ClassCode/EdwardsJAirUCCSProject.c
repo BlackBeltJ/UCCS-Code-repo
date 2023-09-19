d969397aed743ec7a978c2dbd4789350d5c1794a
Joshua Edwards <BlackBeltJJE@gmail.com>
2023-09-19 17:23:46 -0600

    Update EdwardsJAirUCCSProject.c
    Final iteration of main program. Flushed out calculateCharges() function and refined mathematical algorithm. Added bells and whistles to get the program looking nice.

--- CS2060ClassCode/EdwardsJAirUCCSProject.c
+++ CS2060ClassCode/EdwardsJAirUCCSProject.c
@@ -1,11 +1,11 @@
 /*
 Joshua Edwards
 CS 2060 003 AirUCCS Project Iteration 1
-This program creates a system for user to view information about rental properties and 
+This program creates a system for user to view information about rental properties and
 then input the number of nights they want to rent the property for and receive an estimated
-charge based on the number of nights and the discount rate. The owner of the property can 
-also input a special value to receive back a summary of the nights rented and the total 
-money made. 
+charge based on the number of nights and the discount rate. The owner of the property can
+also input a special value to receive back a summary of the nights rented and the total
+money made.
 */
 
 #include <stdio.h>
@@ -45,13 +45,16 @@ int main(void) {
 	while (userNightInput != SENTINAL_NEG1) {
 		printRentalPropertyInfo(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, INTERVAL_1_NIGHTS, INTERVAL_2_NIGHTS, RENTAL_RATE, DISCOUNT);
 		userNightInput = getValidInt(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, SENTINAL_NEG1);
-		double rentalCharge = calculateCharges(userNightInput, INTERVAL_1_NIGHTS, INTERVAL_2_NIGHTS, RENTAL_RATE, DISCOUNT);
-		totalCharges = totalCharges + rentalCharge;
-		totalNights = totalNights + userNightInput;
+		if (userNightInput != -1)
+		{
+			double rentalCharge = calculateCharges(userNightInput, INTERVAL_1_NIGHTS, INTERVAL_2_NIGHTS, RENTAL_RATE, DISCOUNT);
+			totalCharges = totalCharges + rentalCharge;
+			totalNights = totalNights + userNightInput;
+		}
 	}
 	if (totalNights == 0 || totalCharges == 0)
 	{
-		puts("No nights and charges accrewed.");
+		puts("\nThere were no rentals...Exiting program");
 	}
 	else
 	{
@@ -62,10 +65,10 @@ int main(void) {
 //Prints the rental property information 
 void printRentalPropertyInfo(unsigned int minNights, unsigned int maxNights, unsigned int
 	interval1Nights, unsigned int interval2Nights, double rate, double discount) {
-	printf("Rental property can be rented for %d to %d nights", minNights, maxNights);
-	printf("%lf rate a night for the first %d nights", rate, interval1Nights);
-	printf("%lf discount rate a night for nights %d to %d", discount, interval1Nights, interval2Nights);
-	printf("%lf discount rate a night for each remaining night over %d", (rate * 2), interval2Nights);
+	printf("\n\nRental property can be rented for %d to %d nights", minNights, maxNights);
+	printf("\n%.2f rate a night for the first %d nights", rate, interval1Nights);
+	printf("\n%.2f discount rate a night for nights %d to %d", discount, (interval1Nights + 1), interval2Nights);
+	printf("\n%.2f discount rate a night for each remaining night over %d.\n", (discount * 2), interval2Nights);
 }
 
 //returns only valid input from the user based on the min and max values inclusively
@@ -75,12 +78,14 @@ int getValidInt(int min, int max, int sentinal) {
 
 	// algorithm to check valid input
 	bool isValidInput = false;
-	while ((getchar() != '\n')); // clears the input buffer
+
+	puts("\nEnter the number of nights you want to rent the property: ");
+	
 	while (isValidInput == false)
 	{
-		puts("Enter the number of nights you want to rent the property: ");
+		// prompt user for nights
 		scanfReturnValue = scanf("%d", &userNightInput);
-		while ((getchar() != '\n')); // clear the input buffer again
+		while ((getchar() != '\n')); // clear the input buffer 
 
 		if (scanfReturnValue == 1)
 		{
@@ -90,16 +95,19 @@ int getValidInt(int min, int max, int sentinal) {
 			{
 				isValidInput = true;
 			}
+			else if (userNightInput == -1) {
+				isValidInput = true;
+			}
 			else
 			{
-				printf("Error: Not within %d and %d. Please enter the value again.\n ", min, max);
+				printf("Error: Not within %d and %d. Please enter the value again:", min, max);
 				isValidInput = false;
 			}
 		}
 		else
 		{
 			// prompt the user for an integer again if they did not enter one initially
-			puts("Error: Not an integer number. Please enter the value again.\n");
+			printf("Error: Not an integer number. Please enter the value again:\n");
 			isValidInput = false;
 		}
 	}
@@ -111,30 +119,29 @@ double calculateCharges(unsigned int nights, unsigned int interval1Nights, unsig
 	interval2Nights, double rate, double discount) {
 	double charge = 0;
 
-	// create math logic
-	// base rate
-	if (nights < interval1Nights) {
-		charge += (nights * rate);
+	// math logic
+	charge = nights * rate; // base case nights is less than interval 1
+	if (nights > interval1Nights) { // second case nights are between interval 1 and 2 (standard discount)
+		if (nights > interval2Nights) 
+		{
+			charge = charge - ((interval2Nights - interval1Nights) * discount);
+		}
+		else {
+			charge = charge - ((nights - interval1Nights) * discount);
+		}
 	}
-	// reduced rate
-	if (nights > interval2Nights) {
-		charge += ((interval2Nights - interval1Nights) * (rate - discount));
+	if (nights > interval2Nights) { // third case nights are over interval 2 (double discount)
+		charge = charge - ((nights - interval2Nights) * (discount * 2));
 	}
-	else (nights <= interval2Nights); 
-		charge += ((nights - interval1Nights) * (rate * discount));
-	// double reduced rate
-	if (nights > interval2Nights) {
-		charge += ((nights - interval2Nights) * (rate - 2 * discount));
-	}
-	// need to increment charge as it's calculated
+	printf("\nNights\t\tCharge\n%d\t\t$%.2f", nights, charge);
 	return charge;
 }
 
 //Prints the number of nights, and the charge  if there were any rentals
 //Use for printing  vacationer charge or for property owner summary
 void printNightsCharges(unsigned int nights, double charges) {
-	puts("Rental Property Owner Total Summary");
-	printf("\n\nNights\tCharge\n%d\t%lf.2", nights, charges);
+	puts("\nRental Property Owner Total Summary");
+	printf("\nNights\tCharge\n%d\t$%.0f\n", nights, charges);
 }
 
 
