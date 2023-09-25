1761e7efe2eda14bd9a5a07125fe84933c3a9543
Joshua Edwards <BlackBeltJJE@gmail.com>
2023-09-25 16:22:26 -0600

    lol I found the errors with the preprocessor
    I did not need to be passing the #define constants directly in the function prototype. I named them to something else and the errors went away. I will pass the preprocessor defs in main when the function is called.

--- CS2060ClassCode/GE04RenterSurveyEdwards.c
+++ CS2060ClassCode/GE04RenterSurveyEdwards.c
@@ -1,7 +1,7 @@
-/* 
+/*
 Joshua Edwards
 GE04 Renter Survey code
-This program allows users to leave reviews on AirUCCS properties and then will allow the 
+This program allows users to leave reviews on AirUCCS properties and then will allow the
 users to see past reviews and allow renters to see the average review score for each category.
 */
 
@@ -17,9 +17,9 @@ users to see past reviews and allow renters to see the average review score for 
 
 // insert function prototypes here
 void printCategories(const char* categories[], size_t totalCategories);
-void getRatings(int rentalSurvey[][RENTERRATING], size_t renters_rows, size_t ratings_colums, int MINRATING, int MAXRATING);
-int getValidInt(int MINRATING, int MAXRATING);
-void printSurveyResults(const int rentalSurvey[][RENTERRATING], int MINRATING, int MAXRATIN);
+void getRatings(int rentalSurvey[][RENTERRATING], size_t renters_rows, size_t ratings_columns, int MIN_RATING, int MAX_RATING);
+int getValidInt(int MIN_RATING, int MAX_RATING);
+void printSurveyResults(const int rentalSurvey[][RENTERRATING], int MIN_RATING, int MAX_RATING);
 void calculateCetegoryAverages(int rentalSurvey[][RENTERRATING], int categoryAverages[RENTER_SURVEY_CATEGORIES], size_t renters_rows, size_t ratings_columns);
 void printCategoryData(const int categoryAverages[RENTER_SURVEY_CATEGORIES], size_t totalCategories);
 
@@ -54,19 +54,19 @@ void printCategories(const char* categories[], size_t totalCategories)
 	puts(""); // start new line of output
 }
 
-void getRatings(int rentalSurvey[][RENTERRATING], size_t renters_rows, size_t ratings_colums, int MIN_RATING, int MAX_RATING);
+void getRatings(int rentalSurvey[][RENTERRATING], size_t renters_rows, size_t ratings_colums, int MIN_RATING, int MAX_RATING) {
 	// display character array for rating titles
 	// ask user for each rating	
 	// verify valid data, use getValidInt()
 	// write data to rentalSurvey[][] array
 }
 
-int getValidInt(MINRATING, MAXRATING) {
+int getValidInt(int MIN_RATING, int MAX_RATING) {
 	// grab algorithm from documentation
 	// check for range
 }
 
-void printSurveyResults(const int rentalSurvey[][RENTERRATING], int MINRATING, int MAXRATIN) {
+void printSurveyResults(const int rentalSurvey[][RENTERRATING], int MIN_RATING, int MAX_RATING) {
 	// loop through array and display the each row and column
 }
 
