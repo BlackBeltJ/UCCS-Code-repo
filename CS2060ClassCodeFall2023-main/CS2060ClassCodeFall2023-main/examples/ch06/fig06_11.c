f6ae2fc2122753cc8c48f6e5f223958ab73d25f7
Joshua Edwards <BlackBeltJJE@gmail.com>
2023-09-25 18:34:21 -0600

    Update EdwardsJ_ITER01_revised and fig06_11.c

--- CS2060ClassCodeFall2023-main/CS2060ClassCodeFall2023-main/examples/ch06/fig06_11.c
+++ CS2060ClassCodeFall2023-main/CS2060ClassCodeFall2023-main/examples/ch06/fig06_11.c
@@ -5,29 +5,28 @@
 
 // function prototypes
 void modifyArray(int b[], size_t size); 
-void modifyElement(int e);              
+void modifyElement(int e);      
+void displayArray(const int b[], size_t size);
  
 // function main begins program execution
 int main(void) {
    int a[SIZE] = {0, 1, 2, 3, 4}; // initialize array a
 
-   puts("Effects of passing entire array by reference:\n\nThe "
+   printf("Value of array name: %d", a);
+   printf("\nMemory address of first element: %d", &a[0]);
+
+
+   puts("\n\nEffects of passing entire array by reference:\n\nThe "
       "values of the original array are:");
 
-   // output original array
-   for (size_t i = 0; i < SIZE; ++i) { 
-      printf("%3d", a[i]);
-   } 
+   displayArray(a, SIZE);
 
    puts(""); // outputs a newline
 
    modifyArray(a, SIZE); // pass array a to modifyArray by reference
    puts("The values of the modified array are:");
 
-   // output modified array
-   for (size_t i = 0; i < SIZE; ++i) {
-      printf("%3d", a[i]);
-   } 
+   displayArray(a, SIZE);
 
    // output value of a[3]
    printf("\n\n\nEffects of passing array element "
@@ -54,6 +53,12 @@ void modifyElement(int e) {
    printf("Value in modifyElement is %d\n", e);            
 } 
 
+void displayArray(const int b[], size_t size) {
+    // output passed array
+    for (size_t i = 0; i < size; ++i) {
+        printf("%3d", b[i]);
+    }
+}
 
 
 /**************************************************************************
